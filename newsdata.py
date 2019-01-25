2ce00613ce6e422c7393a49310eca48536b9f8db
CBStanley12 <cbstanley12@gmail.com>
2019-01-25 14:25:12 -0700

    Refactored SQL queries

--- newsdata.py
+++ newsdata.py
@@ -6,21 +6,21 @@ DBNAME = "news"
 
 
 def pop_articles():
-    """Returns the top 3 most popular articles (by page views)"""s
+    """Returns the top 3 most popular articles (by page views)"""
     db = psycopg2.connect(dbname=DBNAME)
     c = db.cursor()
-    c.execute("""select title, count(log.path) as num from articles,
-            log where (articles.slug = substring(log.path, 10))
-            group by title order by num desc limit 3;""")
+    c.execute("""SELECT title, COUNT(log.path) AS num
+            FROM articles, log
+            WHERE (articles.slug = SUBSTRING(log.path, 10))
+            GROUP BY title
+            ORDER BY num DESC
+            LIMIT 3;""")
     res = c.fetchall()
     db.close()
-    with open("./log_results.txt", "a") as f:
+    with open("./log_results.txt", "w") as f:
         f.write("\n\nTop 3 Most Popular Articles:")
-        i = 0
-        while i < len(res):
-            f.write('\n{}. "{}" - {} views'.format((i+1),
-                    res[i][0], res[i][1]))
-            i += 1
+        for i, (title, views) in enumerate(res, 1):
+            f.write('\n{}. "{}" - {} views'.format(i, title, views))
         f.close()
 
 
@@ -28,19 +28,22 @@ def pop_authors():
     """Returns the most popular authors (by page views)"""
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("""select authors.name, sum(subq.num) as total from authors,
-            articles, (select title, author, count(log.path) as num from
-            articles, log where (articles.slug = substring(log.path, 10))
-            group by title, author order by num desc) as subq where authors.id
-            = articles.author group by authors.name order by total desc;""")
+    c.execute("""SELECT authors.name, sum(DISTINCT subq.num) AS total
+            FROM authors,
+                (SELECT title, author, count(log.path) AS num
+                FROM articles, log
+                WHERE (articles.slug = substring(log.path, 10))
+                GROUP BY title, author
+                ORDER BY num DESC) AS subq
+            WHERE authors.id = subq.author
+            GROUP BY authors.name
+            ORDER BY total DESC;""")
     res = c.fetchall()
     db.close()
     with open("./log_results.txt", "a") as f:
         f.write("\n\nMost Popular Authors: ")
-        i = 0
-        while i < len(res):
-            f.write("\n{}. {} - {} views".format((i+1), res[i][0], res[i][1]))
-            i += 1
+        for i, (name, views) in enumerate(res, 1):
+            f.write("\n{}. {} - {} views".format(i, name, views))
         f.close()
 
 
@@ -48,23 +51,25 @@ def req_errors():
     """Returns the day(s) on which more than 1% of requests lead to errors"""
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("""select time::date, round( 100.0 * (
-            sum(case when status = '404 NOT FOUND' then 1 else 0 end)::decimal
-            / count(status)), 1 ) as perc_err from log group by time::date
-            order by perc_err desc;""")
+    c.execute("""SELECT *
+            FROM
+                ( SELECT time::DATE,
+                    round( 100.0 * ( sum(
+                        CASE
+                            WHEN status = '404 NOT FOUND' THEN 1
+                            ELSE 0
+                        END )::DECIMAL / COUNT(status)), 1) AS perc_err
+                FROM log
+                GROUP BY time::DATE
+                ORDER BY perc_err DESC ) AS subq
+            WHERE perc_err > 1.0;""")
     res = c.fetchall()
     db.close()
     with open("./log_results.txt", "a") as f:
         f.write("\n\nDays Where More Than 1% of Requests Lead to Errors:")
-        i = 0
-        while i < len(res):
-            if res[i][1] > 1.0:
-                f.write(u"\n\u2022 {} - {}% errors".format(res[i][0].strftime
-                        ('%B %d, %Y'), str(res[i][1])).encode('utf-8'))
-                i += 1
-            else:
-                i += 1
-        f.close()
+        for date, perc_err in res:
+            f.write(u"\n\u2022 {} - {}% errors".format(date.strftime
+                    ('%B %d, %Y'), str(perc_err)).encode('utf-8'))
 
 
 if __name__ == '__main__':
